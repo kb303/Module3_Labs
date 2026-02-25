f7c7d6dbfc5cd36c97c4b7a83c4e80c486aa20fb
kb303 <kirstenbrown99@gmail.com>
2026-02-25 19:55:51 +1100

    Q4 of JS Fundamentals

---
+++ JS Fundamentals/Q4.js
@@ -0,0 +1,12 @@
+let a = 2,
+  b = 3;
+let result = `${a} + ${b} is `;
+// if (a + b < 10) {
+//   result += "less than 10";
+// } else {
+//   result += "greater than 10";
+// }
+
+result += a + b < 10 ? "less than 10" : "greater than 10";
+
+console.log(result);
