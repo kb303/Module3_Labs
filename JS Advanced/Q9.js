52c708ce62ed797ebafa5eae73054d314130cb07
kb303 <kirstenbrown99@gmail.com>
2026-04-11 11:59:45 +1000

    Q9

--- JS Advanced/Q9.js
+++ JS Advanced/Q9.js
@@ -1,4 +1,15 @@
 function randomDelay() {
-  let promise = new Promise(function (resolve, reject) {});
+  return new Promise(function (resolve, reject) {
+    const delay = Math.floor(Math.random() * 19 + 1) * 1000;
+    if (delay % 2 !== 0) {
+      reject(delay);
+    } else {
+      setTimeout(resolve, delay);
+    }
+  });
 }
-randomDelay().then(() => console.log("There appears to have been a delay."));
+randomDelay()
+  .then(() => console.log("There appears to have been a delay."))
+  .catch((error) =>
+    console.log("The delay is odd. Value of delay: " + error + "ms. "),
+  );
